6948e4d07d8b685352472ace845f0f25afbe7e2c
Noah Villanueva <nvilla4@students.towson.edu>
2023-05-13 05:39:34 -0400

    Recommit with added comments

--- app/src/main/java/com/example/nome/ui/theme/globalStateDataClass.kt
+++ app/src/main/java/com/example/nome/ui/theme/globalStateDataClass.kt
@@ -1,2 +1,17 @@
 package com.example.nome.ui.theme
 
+import androidx.compose.runtime.MutableState
+
+/*when sound is playing and screen is changed. it goes back to default mustableState. Bpm 60 and active button changed
+//Resolve Idea
+Create a globalDataClass to save and update whenever screen changes. State<Boolean> Slider<Float> BPM<Int>
+we can use this globalDataclass to set te bpm and shit when user clicks on preset.
+Body() is called n presetnvgraph.kt
+create globalStateDataClass. Add parameters to body function to grab globalStateDataClass.
+Edit Body() to update all */
+
+data class globalStateDataClass(
+    var State: Boolean,
+    var Bpm: Int,
+    var Slider: Float
+)
