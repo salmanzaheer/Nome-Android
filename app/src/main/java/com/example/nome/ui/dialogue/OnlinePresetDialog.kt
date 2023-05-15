1c32db46e25ef8c3ad2e46a6077698c4a6574a91
Christopher Biekeu <cbieke1@students.towson.edu>
2023-05-15 00:00:52 -0400

    dialogue

--- app/src/main/java/com/example/nome/ui/dialogue/OnlinePresetDialog.kt
+++ app/src/main/java/com/example/nome/ui/dialogue/OnlinePresetDialog.kt
@@ -3,9 +3,7 @@ package com.example.nome.ui.dialogue
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+
 
 class OnlinePresetDialog(private val context: Context) {
 
@@ -13,12 +11,9 @@ class OnlinePresetDialog(private val context: Context) {
         val builder: AlertDialog.Builder = AlertDialog.Builder(context)
         builder.setMessage("You are leaving the app going to YouTube. Are you sure?")
         builder.setPositiveButton("Sure", DialogInterface.OnClickListener { dialog, which ->
-            // User clicked Sure, start intent to open YouTube
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com"))
-            context.startActivity(intent)
+
         })
         builder.setNegativeButton("Bring me back", DialogInterface.OnClickListener { dialog, which ->
-            // User clicked Bring me back, do nothing
         })
         builder.show()
     }
